94041d657de34eb87fe9316e01d473ef7ef72d2e
Dung Bien WX515244 <dung.bien@huawei.com>
2022-05-19 12:06:12 +0300

    Add project files

---
+++ entry/src/main/java/com/hmos/wear/helloworld/java/MyApplication.java
@@ -0,0 +1,10 @@
+package com.hmos.wear.helloworld.java;
+
+import ohos.aafwk.ability.AbilityPackage;
+
+public class MyApplication extends AbilityPackage {
+    @Override
+    public void onInitialize() {
+        super.onInitialize();
+    }
+}
---
+++ entry/src/test/java/com/hmos/wear/helloworld/java/ExampleTest.java
@@ -0,0 +1,9 @@
+package com.hmos.wear.helloworld.java;
+
+import org.junit.Test;
+
+public class ExampleTest {
+    @Test
+    public void onStart() {
+    }
+}
